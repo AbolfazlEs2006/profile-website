29d91a856f64c6ad33c8c263847b53e2e445beb6
abolfazl <abolfazl@gmail.com>
2024-10-13 02:35:04 +0330

    responsive web site and landing about me page

--- js/app.js
+++ js/app.js
@@ -5,10 +5,17 @@ let $ = document;
 let navsLink = $.querySelector(".section-1");
 let sectionTwo = $.querySelector(".section-2");
 let sectionThree = $.querySelector(".section-3");
+const closeBtn = document.querySelector('.closeBtn')
+const closeBtnElem = document.getElementById('closeBtn')
+const openNav = document.getElementById('openNav')
+const navbar = document.querySelector('.linkNavbar')
 
 navItems.forEach(function (items) {
   navsLink.insertAdjacentHTML("beforeend",'<a class="active-select" href="./items.html?id=' + items.id + '"><div class="about-me rounded shadow-sm text-center"><span class="icon-nav">'+ items.icon +'</span><h1>' + items.title + '</h1></div></a>'
   );
+  navbar.insertAdjacentHTML('beforeend', `          <a href="./items.html?id=${items.id}">
+            <li>${items.title}${items.icon}</li>
+          </a>`)
 });
 
 let section2 = [
@@ -20,6 +27,13 @@ section2.forEach(function (section2) {
   sectionTwo.insertAdjacentHTML('beforeend','<img src="'+section2.img+'" alt="profile-img" /><h1>'+section2.titleMe+'</h1><p>توسعه دهنده وب سایت های پویا</p><p class="p-about">Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero quas fugitmolestiae fuga laudantium animi! Ratione, iusto aspernatur. Quia, quod!</p><button><a href="./assets/resumePdf/resumepdf.pdf" download style="color: white;">دانلود رزومه</a></button><div class="social-icons"><a><i class="bi bi-instagram"></i></a><a><i class="bi bi-telegram"></i></a><a><i class="bi bi-github"></i></a><a><i class="bi bi-twitter"></i></a><a><i class="bi bi-google"></i></a></div>')
 })
 
-sectionThree.insertAdjacentHTML('beforeend', '     <div><div class="sec-3"><h1>0 1</h1><h6>درباره من</h6><h2></h2><p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و بااستفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی مسخت تایپ به پایانرسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالاتپیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p></div><div class="aboutMe"><h3>نام و نام خانوادگی: <span>ابوالفضل استیری</span></h3><h3>تاریخ تولد: <span>5 فروردین 1385</span></h3><h3>آدرس : <span>ایران ، تهران</span></h3><h3><span>Abolfazl715800@gmail.com</span>: ایمیل</h3></div></div>')
+sectionThree.insertAdjacentHTML('beforeend', '     <div><div class="sec-3"><h1>0 1</h1><h6>درباره من</h6><h2></h2><p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و بااستفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله درستون و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی مسخت تایپ به پایانرسد و زمان مورد نیاز شامل حروفچینی دستاوردهای اصلی، و جوابگوی سوالاتپیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p></div><div class="aboutMe"><h3>نام و نام خانوادگی: <span>ابوالفضل استیری</span></h3><h3>تاریخ تولد: <span>5 فروردین 1385</span></h3><h3>آدرس : <span>ایران ، تهران</span></h3><h3>ایمیل: <span>Abolfazl715800@gmail.com</span></h3></div></div>')
 
 document.title = 'Abolfazl estiri'
+
+openNav.addEventListener('click', () => {
+  closeBtn.classList.toggle('visibility')
+})
+closeBtnElem.addEventListener('click', () => {
+  closeBtn.classList.toggle('visibility')
+})
--- js/items.js
+++ js/items.js
@@ -1,32 +1,110 @@
 let $ = document;
 
 let arrProduct = [
-    {id: 1,img: './assets/img/onlineshop-lg.png', title: 'نمونه سایت فروشگاهی',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2024/8/26'},
-    {id: 2,img: './assets/img/onlineshop-lg.png', title: 'نمونه سایت انگلیسی',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2024/5/13'},
-    {id: 3,img: './assets/img/onlineshop-lg.png', title: 'سایت با استفاده از فریمورک sass',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2023/7/2'},
-    {id: 4,img: './assets/img/onlineshop-lg.png', title: 'سایت با استفاده از فریمورک sass',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2022/6/14'},
+    {id: 1,img: './assets/img/divarresume.png', title: 'ریفکتور سایت دیوار',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2024/8/26', links: 'http://divar.pythonanywhere.com'},
+    {id: 2,img: './assets/img/profileimg.png', title: 'نمونه سایت پروفایل',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2024/5/13'},
+    {id: 3,img: './assets/img/sassproject.png', title: 'سایت با استفاده از فریمورک sass',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2023/7/2'},
+    {id: 4,img: './assets/img/shops.png', title: 'سایت انلاین شاپ و یه نمونه طراحی قالب است',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2022/6/14'},
     {id: 5,img: './assets/img/onlineshop-lg.png', title: 'سایت ویدیویی مانند نماوا',about: `این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است ©`, paragragh: `شما با کلیک روی هر عکس میتونید وب سایت را تماشا کنید و ممنون میشم نظر خود را راجب اون سایت بدهید :)`, time: '2024/8/13'},
 ]
+let myarr = [{
+    age: 18,
+    Gender: 'مرد',
+    address: 'تهران-تهران',
+    MaritalStatus: 'مجرد',
+    Military: 'معافیت تحصیلی',
+    mobile: 4551158,
+    email: 'Abolfazl715800@gmail.com',
+    link: 'http://divar.pythonanywhere.com/',
+    univercity: 'فیزیک مهندسی',
+    lang: 'انگلیسی متوسط',
+}]
 
 let Products = $.querySelector('.card-resume')
 let backBtn = $.getElementById('btn-nav')
+const aboutMe = $.querySelector('.info')
 
 let locationParams = new URLSearchParams(location.search)
 let mainParamsID = locationParams.get('id')
 
-if (mainParamsID == 3) {
+if (mainParamsID == 1) {
+    myarr.forEach(info => {
+        aboutMe.insertAdjacentHTML('beforeend', `        <div class="myInfo">
+            <img src="./assets/img/myimg.jpg" alt="my profile">
+            <h3 class="yourName">ابوالفضل استیری</h3>
+            <div class="aboutMe">
+                    <h3>مشخصات فردی</h3>
+                    <h4>سن: <span>${info.age}</span></h4>
+                    <h4>جنسیت: <span>${info.Gender}</span></h4>
+                    <h4>محل سکونت: <span>${info.address}</span></h4>
+                    <h4>وضعیت تاهل: <span>${info.MaritalStatus}</span></h4>
+                    <h4>نظام وظیقه: <span>${info.Military}</span></h4>
+                </div>
+                <div class="contantUs">
+                    <h3>اطلاعات تماس</h3>
+                    <h4>تلقن: <span>${info.mobile}</span></h4>
+                    <h4>ایمیل: <span>${info.email}</span></h4>
+                </div>
+                <div class="jobs">
+                    <h3>نمونه کار ها</h3>
+                    <a href="${info.link}"><span>${info.link}</span></a>
+                </div>
+        </div>
+        <div class="allAboutMe">
+            <div class="univercity">
+                    <h2>سوابق تحصیلی</h2>
+                    <h3>${info.univercity}</h3>
+                </div>
+                <div class="jobs">
+                    <h2>دوره های آموزشی</h2>
+                    <h3>SQL SERVER <span>مدت دوره: 6ماه</span></h3>
+                    <p>کندو <span>: نام اموزشگاه</span></p>
+                </div>
+                <div class="getintouch">
+                    <h2>زیان های خارجه</h2>
+                    <h3>انگلیسی متوسط</h3>
+                </div>
+                <div class="skill">
+                    <h2>نرم افزار ها</h2>
+                    <h3>Microsoft Word</h3>
+                    <h3>Microsoft Powerpoint</h3>
+                    <h3>Adobe Photoshop</h3>
+                    <h3>Microsoft Excel</h3>
+                </div>
+                <div class="skills">
+                <h2>مهارت های تکمیلی</h2>
+                    <div></div>
+                    <h3>SQL Server</h3>
+                    <h3>Responsive design</h3>
+                    <h3>طراحی تمامی وب سایت های پویا</h3>
+                    <h3>Html</h3>
+                    <h3>css</h3>
+                    <h3>Tailwind css</h3>
+                    <h3>Bootstrap</h3>
+                    <h3>Sass</h3>
+                    <h3>javasript</h3>
+                    <h3>Rest api</h3>
+                    <h3>Git</h3>
+                    <h3>GitHub</h3>
+                    <h3>GitLab</h3>
+                    <h3>Wordpress</h3>
+                </div>
+        </div> <style>.card-resume{display: none;}</style>`)
+    })
+    
+} else if (mainParamsID == 3) {
     arrProduct.forEach(item => {
         Products.insertAdjacentHTML('beforeend', `<div class="about-cards">
             <div>
-                <a href="#">
-                    <img src="${item.img}" alt="resume/site">
+                <a href="${item.links}">
+                    <img style="object-fit: cover; -o-object-fit: content;" class: "img-fluid" src="${item.img}" alt="resume/site">
                 </a>
             </div>
             <div>
                 <div>
                     <div class="info-card">
                         <h2>${item.title}</h2>
-                        <h4> این سایت صرفا برای رزومه است و همچین سایتی برای شخصی ساخته و تولید نشده است &copy</h4>
+                        <h4> این سایت صرفا برای رزومه است و همچین سایتی برای شخص یا سازمانی ساخته و تولید نشده است &copy</h4>
                         <p>${item.paragragh}</p> 
                     </div>
                     <hr>
@@ -39,6 +117,7 @@ if (mainParamsID == 3) {
         </div>`)
     })
 }
+
 
 backBtn.addEventListener('click', () => {
     history.back()
