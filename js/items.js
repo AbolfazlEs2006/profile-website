80c312612cd95db9fd3871b5f0ba010b6365f7ba
abolfazl <abolfazl@gmail.com>
2024-10-24 12:37:18 +0330

    update Modal alert

--- js/items.js
+++ js/items.js
@@ -192,13 +192,12 @@ let passLoginD = $.querySelector('.passLoginD')
 let BtnD = $.querySelector('.btbD')
 
 BtnD.addEventListener('click', () => {
-    if (userNameD.value === '1' || passLoginD.value === '2') {
+    if (userNameD.value === 'dev2006' || passLoginD.value === '6580Es') {
         fetch('https://myresumeprofile-477f9-default-rtdb.firebaseio.com/Massage.json')
             .then(res => res.json())
             .then(data => {
                 let allMassage = Object.entries(data)
                 allMassage.forEach(Massage => {
-                    console.log(Massage[1]);
                     DashboardPannel.insertAdjacentHTML('beforeend', `<style>.Login {display: none;}</style>          <div class='allmassage'>
                         <h2 class="userNameMass">نام کاربری:::::   ${Massage[1].firstname}</h2>
                         <h4 class="emailMass">ایمیل:::::    ${Massage[1].email}</h4>
